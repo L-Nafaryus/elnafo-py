f2d02ad163ce70481f018b4bdcbec90c0ce187e1
L-Nafaryus <l.nafaryus@gmail.com>
2021-03-14 19:13:30 +0500

    git web section

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@
 
 from flask import render_template
 from app import app
+import os
+from git import Repo
 
 @app.route('/')
 @app.route('/index')
@@ -20,6 +22,39 @@ def index():
         {
             "url": "/files",
             "desc": "Files"
+        },
+        {
+            "url": "/git",
+            "desc": "Git"
         }
     ]
+
     return render_template("index.html", title = title, links = links)
+
+@app.route("/git")
+def git():
+    title = "elnafo > git"
+    gitdir = os.path.join(os.getcwd(), "app/public/git")
+    projectsdirs = os.listdir(gitdir)
+    projects = []
+
+    for pd in projectsdirs:
+        repo = Repo(os.path.join(gitdir, pd))
+        name = pd
+        description = repo.description
+        lastchanged = str(repo.head.commit.authored_datetime)
+
+        projects.append({
+            "name": name,
+            "description": description,
+            "lastchanged": lastchanged
+        })
+
+    return render_template("projects.html", projects = projects)
+
+@app.route("git/<project>")
+def index(project):
+    p = os.path.join(os.getcwd(), "app/public/git", project)
+    repo = Repo(p)
+
+    return render_template("repository.html")
